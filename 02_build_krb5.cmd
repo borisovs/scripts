e1d478c22d8530c7f2059c2984518b66c7c6974b
Sergey Borisov <sborisov@astralinux.ru>
2025-11-21 13:54:15 +0300

    [WP-6196] use krb5-1.22.1-final tag

--- 02_build_krb5.cmd
+++ 02_build_krb5.cmd
@@ -13,7 +13,11 @@ set MSYS2_BIN=D:\msys64\usr\bin
 set PATH=%PATH%;"%WindowsSdkVerBinPath%"\x86
 set PATH=%PATH%;%MSYS2_BIN%
 
-set KRB_INSTALL_DIR=%cd%\build
+set SRC_DIR=%cd%\krb5-src\
+if exist %SRC_DIR% rmdir %SRC_DIR% /q /s
+git clone --recursive --depth 1 --single-branch --branch krb5-1.22.1-final https://github.com/krb5/krb5.git %SRC_DIR%
+
+set KRB_INSTALL_DIR=%cd%\krb5
 if exist %KRB_INSTALL_DIR% rmdir %KRB_INSTALL_DIR% /q /s
 mkdir %KRB_INSTALL_DIR%
 
@@ -21,7 +25,7 @@ set OPENSSL_VERSION=3
 set OPENSSL_DIR=%cd%\openssl
 
 @echo on
-cd src
+cd %SRC_DIR%\src
 @REM To skip building the graphical ticket manager
 set NO_LEASH=1
 nmake -f Makefile.in prep-windows
