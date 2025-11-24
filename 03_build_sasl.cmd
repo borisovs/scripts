b58f6ed0863eea133a567596f97019f8a9857b7e
Sergey Borisov <sborisov@astralinux.ru>
2025-11-24 16:18:05 +0300

    [WP-6196] 03_build_sasl.cmd - add KRB5_DIR

--- 03_build_sasl.cmd
+++ 03_build_sasl.cmd
@@ -14,7 +14,7 @@ if exist %SRC_DIR% rmdir %SRC_DIR% /q /s
 git clone --recursive --depth 1 --single-branch --branch cyrus-sasl-2.1.28 https://github.com/borisovs/cyrus-sasl.git %SRC_DIR%
 
 set OPENSSL_DIR=%cd%\openssl
-set Krb5Dir=%cd%\krb5
+set KRB5_DIR=%cd%\krb5
 set SASL_DIR=%cd%\cyrus-sasl\
 if exist %SASL_DIR% rmdir %SASL_DIR% /q /s
 mkdir %SASL_DIR%
@@ -24,6 +24,7 @@ set OPENSSL_LIBPATH=%OPENSSL_DIR%\lib
 powershell -command "(Get-Content -Path '%SRC_DIR%\win32\openssl.props') -replace '\(AdditionalIncludeDirectories\)', '(AdditionalIncludeDirectories);%OPENSSL_DIR%\include' | Set-Content -Path '%SRC_DIR%\win32\openssl.props'"
 powershell -command "(Get-Content -Path '%SRC_DIR%\win32\openssl.props') -replace '\(AdditionalLibraryDirectories\)', '(AdditionalLibraryDirectories);%OPENSSL_DIR%\lib' | Set-Content -Path '%SRC_DIR%\win32\openssl.props'"
 powershell -command "(Get-Content -Path '%SRC_DIR%\win32\openssl.props') -replace 'libeay32', 'libcrypto' | Set-Content -Path '%SRC_DIR%\win32\openssl.props'"
+powershell -command "(Get-Content -Path '%SRC_DIR%\win32\cyrus-sasl.props') -replace '<Krb5Dir>C:\msvc\krb</Krb5Dir>', '<Krb5Dir>%KRB5_DIR%</Krb5Dir>' | Set-Content -Path '%SRC_DIR%\win32\cyrus-sasl.props'"
 
 cd %SRC_DIR%\win32
 @REM msbuild cyrus-sasl-common.sln /p:PlatformToolset=v143 /p:WindowsTargetPlatformVersion=10.0.26100.0  /p:Configuration=Release
